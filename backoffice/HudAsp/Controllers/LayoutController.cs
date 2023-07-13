25e47d7ff6070b49cab93b49534586ac6d2647c9
Suporte KeL Tecnologia <suporte@keltecnologia.com.br>
2023-07-13 08:11:34 -0300

    Add solution project

---
+++ backoffice/HudAsp/Controllers/LayoutController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HudAsp.Models;
+
+namespace HudAsp.Controllers;
+
+public class LayoutController : Controller
+{
+		public IActionResult StarterPage()
+		{
+				return View();
+		}
+
+		public IActionResult FixedFooter()
+		{
+				return View();
+		}
+
+		public IActionResult FullHeight()
+		{
+				return View();
+		}
+
+		public IActionResult FullWidth()
+		{
+				return View();
+		}
+
+		public IActionResult BoxedLayout()
+		{
+				return View();
+		}
+
+		public IActionResult CollapsedSidebar()
+		{
+				return View();
+		}
+
+		public IActionResult TopNav()
+		{
+				return View();
+		}
+
+		public IActionResult MixedNav()
+		{
+				return View();
+		}
+
+		public IActionResult MixedNavBoxedLayout()
+		{
+				return View();
+		}
+}
