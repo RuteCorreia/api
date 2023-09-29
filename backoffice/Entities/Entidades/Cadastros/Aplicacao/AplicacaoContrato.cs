b0b888d2bc25d6559b11b0d0f1a87236798ac3ee
Raphael Mello <raphael_meira_goncalves@hotmail.com>
2023-09-29 16:29:59 -0300

    finalização de criação de entidades

---
+++ backoffice/Entities/Entidades/Cadastros/Aplicacao/AplicacaoContrato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades.Cadastros.Aplicacao
+{
+    public class AplicacaoContrato
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Aplicacao")]
+        public int? IdAplicacao { get; set; }
+        public string? UF { get; set; }
+        public string? Cidade { get; set; }
+        public string? NomeCliente { get; set; }
+        public string? CPFCliente { get; set; }
+        public string? Assinatura { get; set; }
+        public virtual Aplicacao Aplicacao { get; set; }
+    }
+}
---
+++ backoffice/Entities/Entidades/Cadastros/CombateIncendio/CombateIncendioDecolagemPouso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades.Cadastros.CombateIncendio
+{
+    public class CombateIncendioDecolagemPouso
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("CombateIncendio")]
+        public int? IdCombateIncendio { get; set; }
+        public DateTime? DecolagemHorario { get; set; }
+        public string? DecolagemHorimetro { get; set; }
+        public DateTime? PousoHorario { get; set; }
+        public string? PousoHorimetro { get; set; }
+        public virtual CombateIncendio CombateIncendio { get; set; }
+    }
+}
