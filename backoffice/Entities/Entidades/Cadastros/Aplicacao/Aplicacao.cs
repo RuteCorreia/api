bdd142b84e2317d2f22f26fc8ef1e9a74fcfb889
Raphael Mello <raphael_meira_goncalves@hotmail.com>
2023-09-29 13:40:59 -0300

    criação de entidades

---
+++ backoffice/Entities/Entidades/Cadastros/Aplicacao/Aplicacao.cs
@@ -0,0 +1,41 @@
+﻿using Entities.Entidades.Cadastros.Cliente;
+using Entities.Entidades.Cadastros.Cultura;
+using Entities.Entidades.Cadastros.Executores;
+using Entities.Entidades.Cadastros.Pilotos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades.Cadastros.Aplicacao
+{
+    public class Aplicacao
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Empresa")]
+        public int? IdEmpresa { get; set; }
+        public string StatusEnvio { get; set; }
+
+        [ForeignKey("Piloto")]
+        public int? IdPiloto { get; set; }
+
+        [ForeignKey("Executor")]
+        public int? IdExecutor { get; set; }
+
+        [ForeignKey("Cliente")]
+        public int? IdCliente { get; set; }
+
+        [ForeignKey("Cultura")]
+        public int? IdCultura { get; set; }
+
+        public virtual Empresa.Empresa Empresa { get; set; }
+        public virtual Piloto Piloto { get; set; }
+        public virtual Executor Executor { get; set; }
+        public virtual Cliente.Cliente Cliente { get; set; }
+        public virtual Cultura.Cultura Cultura { get; set; }
+
+    }
+}
