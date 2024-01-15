39bf574180e00bdab86716da99aa71f321673000
raphaelmeira.2085@gmail.com <raphaelmeira.2085@gmail.com>
2024-01-15 08:21:58 -0300

    ajuste de url de autenticação de usuario flytec front

--- Flytec Front/Flytec/src/app/_services/auth.service.ts
+++ Flytec Front/Flytec/src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'https://localhost:7221/api/v1/Auth/';
+const AUTH_API = 'https://flytec.keltecnologia.com.br/api/v1/Auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
