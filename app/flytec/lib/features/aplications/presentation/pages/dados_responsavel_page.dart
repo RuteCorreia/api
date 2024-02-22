35920124b02b875e5723e559aa479116c7d5a529
OzielFilho <OzielFilho03@hotmail.com>
2024-02-22 19:21:27 -0300

    feat : remove comments and adjust date report

--- app/flytec/lib/features/aplications/presentation/pages/dados_responsavel_page.dart
+++ app/flytec/lib/features/aplications/presentation/pages/dados_responsavel_page.dart
@@ -85,19 +85,6 @@ class _DadosResponsavelPageState extends State<DadosResponsavelPage> {
   final TextEditingController _cpf = TextEditingController();
   final TextEditingController _telefone = TextEditingController();
   bool verifyFields() {
-    // if (_nome.text.isEmpty) {
-    //   Util.toastAlerta("Insira o nome");
-    //   return false;
-    // } else if (_cpf.text.isEmpty) {
-    //   Util.toastAlerta("Insira o cpf");
-    //   return false;
-    // } else if (_telefone.text.isEmpty) {
-    //   Util.toastAlerta("Insira o telefone");
-    //   return false;
-    // } else if (_data!.assinatura!.isEmpty) {
-    //   Util.toastAlerta("Insira a Assinatura");
-    //   return false;
-    // } else {
       Util.toastSucesso("Dados inseridos com sucesso");
       getIt<GlobalConfigVars>().reportList.last.finalizado = true;
       getIt<GlobalConfigVars>().reportList.last.dadosDoResponsavel =
