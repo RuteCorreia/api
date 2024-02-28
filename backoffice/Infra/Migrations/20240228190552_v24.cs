f46951bbaa08701fa5a76d11b63ad10730cf9a32
Joao Antonio <joao.antonio@keltecnologia.com.br>
2024-02-28 16:07:24 -0300

    migration v23 e 24 (28-02-24)

---
+++ backoffice/Infra/Migrations/20240228190552_v24.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class v24 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Senha",
+                table: "Usuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Senha",
+                table: "Usuario",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
