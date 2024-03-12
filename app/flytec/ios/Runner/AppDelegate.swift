b99403ef4b153b186286b096a9477839fa3649f4
Celestino Lopes <celesti>
2024-03-12 15:37:04 +0100

    release: first ios release

--- app/flytec/ios/Runner/AppDelegate.swift
+++ app/flytec/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAbklSlq1TEwO80M3d505SvzUosay5MSdI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
