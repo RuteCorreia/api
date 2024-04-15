b33375bc163292b412257043b4de83ad47c1f5ea
joaoloboalmeida <joaoalobo01@gmail.com>
2024-04-15 16:39:31 -0300

    add fluxo de edicao dos itens de revisao para manutencao aeronave (15-04-24)

--- backoffice/Domain/Interfaces/Cadastros/ManutencaoAeronaveItemsRevisao/IManutencaoAeronaveItemsRevisaoRepository.cs
+++ backoffice/Domain/Interfaces/Cadastros/ManutencaoAeronaveItemsRevisao/IManutencaoAeronaveItemsRevisaoRepository.cs
@@ -4,7 +4,7 @@ public interface IManutencaoAeronaveItemsRevisaoRepository
 {
     Task AddAsync(IEnumerable<Entidades.Cadastros.ManutencaoAeronaveItemsRevisao.ManutencaoAeronaveItemsRevisao> obj);
     Task UpdateAsync(Entidades.Cadastros.ManutencaoAeronaveItemsRevisao.ManutencaoAeronaveItemsRevisao obj);
-    Task DeleteAsync(int id);
+    Task DeleteByIdManutencaoAeronaveAsync(int id);
     Task<IEnumerable<Entidades.Cadastros.ManutencaoAeronaveItemsRevisao.ManutencaoAeronaveItemsRevisao>> GetAllAsync();
-    Task<Entidades.Cadastros.ManutencaoAeronaveItemsRevisao.ManutencaoAeronaveItemsRevisao> GetByIdAsync(int id);
+    Task<IEnumerable<Entidades.Cadastros.ManutencaoAeronaveItemsRevisao.ManutencaoAeronaveItemsRevisao>> GetAllByManutencaoAeronaveIdAsync(int id);
 }
