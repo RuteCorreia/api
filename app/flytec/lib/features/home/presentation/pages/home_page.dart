6bf4f9b4ec5d6b55c221f22bbb7e134567febdcb
ozielfilho <ozielfilho03@hotmail.com>
2024-04-16 19:50:36 -0300

    feat : showing button of relatorio aplicacao for piloto and executor

--- app/flytec/lib/features/home/presentation/pages/home_page.dart
+++ app/flytec/lib/features/home/presentation/pages/home_page.dart
@@ -461,66 +461,71 @@ class _HomePagaState extends State<HomePaga> {
                     },
                     text: "Minhas Atividades",
                   ),
-                  const SizedBox(height: 10),
-                  CustomActionButton(
-                    imageUrl: "assets/images/edit_icon.svg",
-                    onClick: () {
-                      showAdaptiveDialog<String>(
-                        context: context,
-                        useSafeArea: true,
-                        builder: (BuildContext context) => AlertDialog.adaptive(
-                          insetPadding: const EdgeInsets.all(32),
-                          title: const SizedBox(
-                            width: 244,
-                            height: 30,
-                            child: Text(
-                              'Escolha o tipo de relatório',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 121, 118, 118),
-                                fontSize: 16,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w500,
-                                height: 0.09,
+                  if (getIt<GlobalConfigVars>().userPayload.role == "Piloto" ||
+                      getIt<GlobalConfigVars>().userPayload.role ==
+                          "Executor") ...[
+                    const SizedBox(height: 10),
+                    CustomActionButton(
+                      imageUrl: "assets/images/edit_icon.svg",
+                      onClick: () {
+                        showAdaptiveDialog<String>(
+                          context: context,
+                          useSafeArea: true,
+                          builder: (BuildContext context) =>
+                              AlertDialog.adaptive(
+                            insetPadding: const EdgeInsets.all(32),
+                            title: const SizedBox(
+                              width: 244,
+                              height: 30,
+                              child: Text(
+                                'Escolha o tipo de relatório',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 121, 118, 118),
+                                  fontSize: 16,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w500,
+                                  height: 0.09,
+                                ),
                               ),
                             ),
-                          ),
-                          content: SizedBox(
-                            height: 200,
-                            child: Column(
-                              children: [
-                                CustomDialogButton(
-                                  leftIcon:
-                                      "assets/images/icomoon_free_fire.svg",
-                                  text: "Aplicação",
-                                  onClick: () {
-                                    _reportAplicationController
-                                        ?.setUpdateUpdateView(_updateView);
-                                    context.push("/aplications", extra: {
-                                      'reportAplicationController':
-                                          _reportAplicationController
-                                    });
-                                  },
-                                ),
-                                const SizedBox(height: 10),
-                                CustomDialogButton(
-                                  leftIcon: "assets/images/fire.svg",
-                                  onClick: () {
-                                    context.pop();
-                                    context.push("/combateincendio",
-                                        extra: "dd");
-                                  },
-                                  text: "Combate a incêndio",
-                                )
-                              ],
+                            content: SizedBox(
+                              height: 200,
+                              child: Column(
+                                children: [
+                                  CustomDialogButton(
+                                    leftIcon:
+                                        "assets/images/icomoon_free_fire.svg",
+                                    text: "Aplicação",
+                                    onClick: () {
+                                      _reportAplicationController
+                                          ?.setUpdateUpdateView(_updateView);
+                                      context.push("/aplications", extra: {
+                                        'reportAplicationController':
+                                            _reportAplicationController
+                                      });
+                                    },
+                                  ),
+                                  const SizedBox(height: 10),
+                                  CustomDialogButton(
+                                    leftIcon: "assets/images/fire.svg",
+                                    onClick: () {
+                                      context.pop();
+                                      context.push("/combateincendio",
+                                          extra: "dd");
+                                    },
+                                    text: "Combate a incêndio",
+                                  )
+                                ],
+                              ),
                             ),
+                            actions: const <Widget>[],
                           ),
-                          actions: const <Widget>[],
-                        ),
-                      );
-                    },
-                    text: "Relatório Operacional",
-                  ),
+                        );
+                      },
+                      text: "Relatório Operacional",
+                    ),
+                  ],
                   const SizedBox(height: 10),
                   CustomActionButton(
                     imageUrl: "assets/images/tools.svg",
