7851b195368d9cf33e49af402e562d1f420cf2c4
Raphael Mello <rafael.mello@keltecnologia.com.br>
2024-04-26 12:57:20 -0300

    criação de controller relatorio aplicação para receber dados do app

---
+++ backoffice/Application/DTOs/Cadastros/RelatorioAplicacao/ViewModel/RelatorioAplicacaoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Cadastros.RelatorioAplicacao.ViewModel
+{
+    public class RelatorioAplicacaoViewModel
+    {
+        public int Id { get; set; }
+        public int ContratanteId { get; set; }
+        public int IdentificacaoAreaTratadaId { get; set; }
+        public int CaracteristicasProdutoAplicadoId { get; set; }
+        public int RecomendacoesTecnicasId { get; set; }
+        public int RelatorioAplicacaoId { get; set; }
+        public int ContratoPrestacaoServicoId { get; set; }
+        public int DadosResponsavelId { get; set; }
+        public string Piloto { get; set; }
+        public string Executor { get; set; }
+        public string RefDocument { get; set; }
+        public string Data { get; set; }
+        public string RefUsuario { get; set; }
+    }
+}
---
+++ backoffice/Infra/Repositorio/Cadastros/RelatorioAplicacao/RelatorioAplicacaoRepository.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Interfaces.Cadastros.RelatorioAplicacao;
+using Helpers;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio.Cadastros.RelatorioAplicacao
+{
+    public class RelatorioAplicacaoRepository : IRelatorioAplicacaoRepository
+    {
+        private readonly ContextBase _contextBase;
+
+        public RelatorioAplicacaoRepository(ContextBase contextBase)
+        {
+            _contextBase = contextBase;
+        }
+
+        public async Task AddAsync(Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao obj)
+        {
+            await _contextBase.AddAsync(obj);
+            await _contextBase.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entityToRemove = await GetByIdAsync(id);
+            if (!ObjectNullValidation.IsObjectNull(entityToRemove))
+            {
+                _contextBase.Remove(entityToRemove);
+                await _contextBase.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao>> GetAllAsync()
+        {
+            var entities = await _contextBase.RelatorioAplicacao.ToListAsync();
+            return entities;
+        }
+
+        public async Task<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao> GetByIdAsync(int id)
+        {
+            var obj = await _contextBase.RelatorioAplicacao.FindAsync(id);
+            return obj;
+        }
+
+        public async Task UpdateAsync(Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao obj)
+        {
+            var objeto = await _contextBase.RelatorioAplicacao.FindAsync(obj.Id);
+
+            _contextBase.RelatorioAplicacao.Update(objeto);
+            await _contextBase.SaveChangesAsync();
+        }
+    }
+}
