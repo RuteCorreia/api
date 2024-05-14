e393486c96c844175c5f593e90fea037af9400f9
OzielFilho <OzielFilho03@hotmail.com>
2024-05-14 20:26:59 -0300

    feat : include cnpj empresa in report aplication

--- app/flytec/lib/features/aplications/services/create_aplicacao_report_service.dart
+++ app/flytec/lib/features/aplications/services/create_aplicacao_report_service.dart
@@ -114,7 +114,7 @@ class CreateAplicacaoReportService implements PdfGenerator {
                                             fontSize: 14,
                                             fontWeight: pw.FontWeight.normal)),
                                     pw.Text(
-                                        '${empresa?.numeroEmpresa ?? '000000-0'} - CNPJ 00.000.000/0000-00 - Inscr. Est. ${empresa?.inscricaoEstadualEmpresa ?? '000.000.000.000'}',
+                                        '${empresa?.numeroEmpresa ?? '000000-0'} - CNPJ ${empresa?.cnpj ?? '00.000.000/0000-00'} - Inscr. Est. ${empresa?.inscricaoEstadualEmpresa ?? '000.000.000.000'}',
                                         maxLines: 1,
                                         style: pw.TextStyle(
                                             fontSize: 8,
