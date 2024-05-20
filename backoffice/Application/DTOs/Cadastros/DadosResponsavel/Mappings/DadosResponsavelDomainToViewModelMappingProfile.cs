2dc6a06e7c5e409d9371d6e4c6e6325b760b35be
joaoloboalmeida <joaoalobo01@gmail.com>
2024-05-20 20:50:29 -0300

    adicionando controller de dados responsavel (20-05-24)

---
+++ backoffice/Application/DTOs/Cadastros/DadosResponsavel/Mappings/DadosResponsavelDomainToViewModelMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.Cadastros.DadosResponsavel.ViewModel;
+using AutoMapper;
+
+namespace Application.DTOs.Cadastros.DadosResponsavel.Mappings;
+
+public class DadosResponsavelDomainToViewModelMappingProfile : Profile
+{
+    public DadosResponsavelDomainToViewModelMappingProfile()
+    {
+        CreateMap<Domain.Entidades.Cadastros.DadosResponsavel.DadosResponsavel, DadosResponsavelViewModel>();
+    }
+}
---
+++ backoffice/WebApi/Controllers/APIs/DadosResponsavelController.cs
@@ -0,0 +1,136 @@
+﻿using Application.DTOs.Cadastros.DadosResponsavel.Interface;
+using Application.DTOs.Cadastros.DadosResponsavel.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using WebApi.HttpRequestInfo;
+
+namespace WebApi.Controllers.APIs;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+[Authorize]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+[ProducesResponseType(StatusCodes.Status404NotFound)]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+public class DadosResponsavelController : ControllerBase
+{
+    private readonly LoggedUserInfoService _loggedUserInfoService;
+    private readonly IDadosResponsavelService _dadosResponsavelService;
+
+    public DadosResponsavelController(
+        LoggedUserInfoService loggedUserInfoService,
+        IDadosResponsavelService dadosResponsavelService
+    )
+    {
+        _loggedUserInfoService = loggedUserInfoService;
+        _dadosResponsavelService = dadosResponsavelService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IAsyncEnumerable<DadosResponsavelViewModel>>> GetAll()
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            var cadastros = await _dadosResponsavelService.GetAllAsync(loggedUser.Item3);
+            return Ok(cadastros);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"DadosResponsavel getAll - {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<DadosResponsavelViewModel>> GetById(int id)
+    {
+        var returnMsg = new StringBuilder().Append("Não encontrado");
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            var obj = await _dadosResponsavelService.GetByIdAsync(id, loggedUser.Item3);
+            if (obj is not null)
+                returnMsg.Clear();
+
+            return string.IsNullOrEmpty(returnMsg.ToString()) ? Ok(obj) : StatusCode(StatusCodes.Status404NotFound, returnMsg.ToString());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, returnMsg.Clear().Append($"DadosResponsavel getById - {ex.Message}").ToString());
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Add([FromBody] DadosResponsavelViewModel obj)
+    {
+        var returnMsg = new StringBuilder().Append("Modelo inválido");
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+                await _dadosResponsavelService.AddAsync(obj, loggedUser.Item3);
+                returnMsg.Clear();
+            }
+
+            return string.IsNullOrEmpty(returnMsg.ToString()) ? Ok() : StatusCode(StatusCodes.Status400BadRequest, returnMsg.ToString());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, returnMsg.Clear().Append($"DadosResponsavel add - {ex.Message}").ToString());
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> Update(int id, [FromBody] DadosResponsavelViewModel obj)
+    {
+        var returnMsg = new StringBuilder().Append("Modelo inválido");
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+                var objeto = await _dadosResponsavelService.GetByIdAsync(id, loggedUser.Item3);
+                if (objeto is not null)
+                {
+                    obj.Id = objeto.Id;
+                    await _dadosResponsavelService.UpdateAsync(obj);
+                    returnMsg.Clear();
+                }
+                else
+                {
+                    returnMsg.Clear().Append("Não encontrado");
+                }
+            }
+
+            return string.IsNullOrEmpty(returnMsg.ToString()) ? Ok() : StatusCode(StatusCodes.Status400BadRequest, returnMsg.ToString());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, returnMsg.Clear().Append($"DadosResponsavel update - {ex.Message}").ToString());
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var returnMsg = new StringBuilder().Append("Solicitação não foi possível de ser executada");
+        try
+        {
+            if (id != 0)
+            {
+                var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+                await _dadosResponsavelService.DeleteAsync(id, loggedUser.Item3);
+                returnMsg.Clear();
+            }
+
+            return string.IsNullOrEmpty(returnMsg.ToString()) ? Ok() : StatusCode(StatusCodes.Status400BadRequest, returnMsg.ToString());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, returnMsg.Clear().Append($"DadosResponsavel delete - {ex.Message}").ToString());
+        }
+    }
+}
