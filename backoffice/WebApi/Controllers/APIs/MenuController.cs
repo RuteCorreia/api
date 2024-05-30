b1a62eb6be77f73f5b6e9523b01e551162eccf63
joaoloboalmeida <joaoalobo01@gmail.com>
2024-05-29 22:45:58 -0300

    correção de acesso indevido ao gerenciamento de menu e submenu (29-05-24)

--- backoffice/WebApi/Controllers/APIs/MenuController.cs
+++ backoffice/WebApi/Controllers/APIs/MenuController.cs
@@ -2,42 +2,80 @@
 using Application.DTOs.Cadastros.Menu.ViewModel;
 using Application.DTOs.Cadastros.SubMenu.Interface;
 using Application.DTOs.Log.Interface;
-using AutoMapper;
 using Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.HttpRequestInfo;
 
-namespace WebApi.Controllers.APIs
+namespace WebApi.Controllers.APIs;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+[Authorize]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(StatusCodes.Status404NotFound)]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+public class MenuController : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    [Authorize]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public class MenuController : ControllerBase
+    private readonly IMenuService _menuService;
+    private readonly ISubMenuService _subMenuService;
+    private readonly LoggedUserInfoService _loggedUserInfoService;
+    private readonly ILogService _logService;
+
+    public MenuController(
+        IMenuService menuService, 
+        ISubMenuService subMenuService, 
+        LoggedUserInfoService loggedUserInfoService, 
+        ILogService logService
+    )
     {
-        private readonly IMenuService _menuService;
-        private readonly ISubMenuService _subMenuService;
-        private readonly LoggedUserInfoService _loggedUserInfoService;
-        private readonly ILogService _logService;
+        _menuService = menuService;
+        _subMenuService = subMenuService;
+        _loggedUserInfoService = loggedUserInfoService;
+        _logService = logService;
+    }
 
-        public MenuController(IMenuService menuService, ISubMenuService subMenuService, LoggedUserInfoService loggedUserInfoService, ILogService logService)
+    [HttpGet]
+    public async Task<ActionResult<IAsyncEnumerable<MenuViewModel>>> GetAll()
+    {
+        try
         {
-            _menuService = menuService;
-            _subMenuService = subMenuService;
-            _loggedUserInfoService = loggedUserInfoService;
-            _logService = logService;
-        }
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            var menu = await _menuService.GetAllAsync(loggedUser.Item3);
+            var subMenu = await _subMenuService.GetAllAsync();
 
-        [HttpGet]
-        public async Task<ActionResult<IAsyncEnumerable<MenuViewModel>>> GetAll()
-        {
-            try
+            foreach (var m in menu)
             {
-                var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+                foreach (var submenu in subMenu)
+                {
+                    if (m.MenuItemId == submenu.MenuItemId)
+                    {
+                        m.SubMenus.Add(submenu);
+                    }
+                }
+            }
+
+            _logService.LogInformation("Registros de menu recuperados com sucesso.");
+            return Ok(menu);
+        }
+        catch (Exception ex)
+        {
+            _logService.LogError(ex, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
+        }
+    }
+
+    [HttpGet("menusToManage")]
+    [Authorize(Roles = "Administrador")]
+    public async Task<ActionResult<IAsyncEnumerable<MenuViewModel>>> GetAllToManage()
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if(string.IsNullOrEmpty(loggedUser.Item3))
+            {
                 var menu = await _menuService.GetAllAsync(loggedUser.Item3);
                 var subMenu = await _subMenuService.GetAllAsync();
 
@@ -55,17 +93,25 @@ namespace WebApi.Controllers.APIs
                 _logService.LogInformation("Registros de menu recuperados com sucesso.");
                 return Ok(menu);
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
-            }
-        }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<MenuViewModel>> GetById(int id)
+            _logService.LogWarning($"Acesso não autorizado ao gerenciamento de menus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar todos os registros de menu: {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    [Authorize(Roles = "Administrador")]
+    public async Task<ActionResult<MenuViewModel>> GetById(int id)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 var menu = await _menuService.GetByIdAsync(id);
                 if (!ObjectNullValidation.IsObjectNull(menu))
@@ -76,47 +122,55 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Menu não encontrado.");
                 return StatusCode(StatusCodes.Status404NotFound, "Menu não encontrado");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao recuperar menu pelo ID: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar menu pelo ID: {ex.Message}");
-            }
+
+            _logService.LogWarning($"Acesso não autorizado ao detalhe de menu pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
         }
-
-        [HttpGet("dropdown")]
-        public async Task<ActionResult> GetDropdown()
+        catch (Exception ex)
         {
-            try
-            {
-                var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
-                var menu = await _menuService.GetAllAsync(loggedUser.Item3);
-                var subMenu = await _subMenuService.GetAllAsync();
+            _logService.LogError(ex, $"Erro ao recuperar menu pelo ID: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar menu pelo ID: {ex.Message}");
+        }
+    }
 
-                foreach (var submenu in subMenu)
+    [HttpGet("dropdown")]
+    public async Task<ActionResult> GetDropdown()
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            var menu = await _menuService.GetAllAsync(loggedUser.Item3);
+            var subMenu = await _subMenuService.GetAllAsync();
+
+            foreach (var submenu in subMenu)
+            {
+                var newObj = new MenuViewModel
                 {
-                    var newObj = new MenuViewModel
-                    {
-                        MenuItemId = submenu.SubMenuId,
-                        Label = submenu.Label,
-                    };
+                    MenuItemId = submenu.SubMenuId,
+                    Label = submenu.Label,
+                };
 
-                    menu = menu.Concat(new[] { newObj });
-                }
+                menu = menu.Concat(new[] { newObj });
+            }
 
-                _logService.LogInformation("Menu suspenso recuperado com sucesso.");
-                return Ok(menu);
-            }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao recuperar menu suspenso: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar menu suspenso: {ex.Message}");
-            }
+            _logService.LogInformation("Menu suspenso recuperado com sucesso.");
+            return Ok(menu);
         }
-
-        [HttpPost]
-        public async Task<ActionResult> Add([FromBody] MenuViewModel obj)
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao recuperar menu suspenso: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar menu suspenso: {ex.Message}");
+        }
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Administrador")]
+    public async Task<ActionResult> Add([FromBody] MenuViewModel obj)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 if (ModelState.IsValid)
                 {
@@ -128,17 +182,25 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Modelo de menu inválido.");
                 return StatusCode(StatusCodes.Status400BadRequest, "Modelo de menu inválido");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao adicionar menu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar menu: {ex.Message}");
-            }
-        }
 
-        [HttpPut("{id:int}")]
-        public async Task<ActionResult> Update(int id, [FromBody] MenuViewModel obj)
+            _logService.LogWarning($"Acesso não autorizado à criação de menu pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao adicionar menu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar menu: {ex.Message}");
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    [Authorize(Roles = "Administrador")]
+    public async Task<ActionResult> Update(int id, [FromBody] MenuViewModel obj)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 if (ModelState.IsValid)
                 {
@@ -161,11 +223,14 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Modelo de menu inválido.");
                 return StatusCode(StatusCodes.Status400BadRequest, "Modelo de menu inválido");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao atualizar menu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar menu: {ex.Message}");
-            }
+
+            _logService.LogWarning($"Acesso não autorizado à atualização de menu pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
+        {
+            _logService.LogError(ex, $"Erro ao atualizar menu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar menu: {ex.Message}");
         }
     }
 }
--- backoffice/WebApi/Controllers/APIs/SubMenuController.cs
+++ backoffice/WebApi/Controllers/APIs/SubMenuController.cs
@@ -4,47 +4,65 @@ using Application.DTOs.Log.Interface;
 using Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.HttpRequestInfo;
 
-namespace WebApi.Controllers.APIs
+namespace WebApi.Controllers.APIs;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+[Authorize(Roles = "Administrador")]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(StatusCodes.Status404NotFound)]
+[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+public class SubMenuController : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    [Authorize]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public class SubMenuController : ControllerBase
+    private readonly ISubMenuService _subMenuService;
+    private readonly ILogService _logService;
+    private readonly LoggedUserInfoService _loggedUserInfoService;
+
+    public SubMenuController(
+        ISubMenuService subMenuService, 
+        ILogService logService,
+        LoggedUserInfoService loggedUserInfoService
+    )
     {
-        private readonly ISubMenuService _subMenuService;
-        private readonly ILogService _logService;
+        _subMenuService = subMenuService;
+        _logService = logService;
+        _loggedUserInfoService = loggedUserInfoService;
+    }
 
-        public SubMenuController(ISubMenuService subMenuService, ILogService logService)
+    [HttpGet]
+    public async Task<ActionResult<IAsyncEnumerable<SubMenuViewModel>>> GetAll()
+    {
+        try
         {
-            _subMenuService = subMenuService;
-            _logService = logService;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IAsyncEnumerable<SubMenuViewModel>>> GetAll()
-        {
-            try
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 var subMenu = await _subMenuService.GetAllAsync();
                 _logService.LogInformation("Todos os submenus foram recuperados com sucesso.");
                 return Ok(subMenu);
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao recuperar todos os submenus: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar todos os submenus: {ex.Message}");
-            }
-        }
 
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<SubMenuViewModel>> GetById(int id)
+            _logService.LogWarning($"Acesso não autorizado ao gerenciamento de submenus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao recuperar todos os submenus: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar todos os submenus: {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<SubMenuViewModel>> GetById(int id)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 var subMenu = await _subMenuService.GetByIdAsync(id);
                 if (!ObjectNullValidation.IsObjectNull(subMenu))
@@ -56,17 +74,24 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("SubMenu não encontrado.");
                 return StatusCode(StatusCodes.Status404NotFound, "SubMenu não encontrado");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao recuperar SubMenu pelo ID: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar SubMenu pelo ID: {ex.Message}");
-            }
-        }
 
-        [HttpPost]
-        public async Task<ActionResult> Add([FromBody] SubMenuViewModel obj)
+            _logService.LogWarning($"Acesso não autorizado ao detalhe de submenus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao recuperar SubMenu pelo ID: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao recuperar SubMenu pelo ID: {ex.Message}");
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Add([FromBody] SubMenuViewModel obj)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 if (ModelState.IsValid)
                 {
@@ -78,17 +103,24 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Modelo inválido ao adicionar novo SubMenu.");
                 return StatusCode(StatusCodes.Status400BadRequest, "Modelo inválido");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao adicionar novo SubMenu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar novo SubMenu: {ex.Message}");
-            }
-        }
 
-        [HttpPut("{id:int}")]
-        public async Task<ActionResult> Update(int id, [FromBody] SubMenuViewModel obj)
+            _logService.LogWarning($"Acesso não autorizado à criação de submenus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao adicionar novo SubMenu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao adicionar novo SubMenu: {ex.Message}");
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> Update(int id, [FromBody] SubMenuViewModel obj)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 if (ModelState.IsValid)
                 {
@@ -111,17 +143,24 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Modelo inválido ao atualizar SubMenu.");
                 return StatusCode(StatusCodes.Status400BadRequest, "Modelo inválido");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao atualizar SubMenu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao atualizar SubMenu: {ex.Message}");
-            }
-        }
 
-        [HttpDelete("{id:int}")]
-        public async Task<ActionResult> Delete(int id)
+            _logService.LogWarning($"Acesso não autorizado à atualização de submenus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
         {
-            try
+            _logService.LogError(ex, $"Erro ao atualizar SubMenu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao atualizar SubMenu: {ex.Message}");
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        try
+        {
+            var loggedUser = _loggedUserInfoService.GetLoggedUserIdentityIdAndRole();
+            if (string.IsNullOrEmpty(loggedUser.Item3))
             {
                 if (id != 0)
                 {
@@ -133,11 +172,14 @@ namespace WebApi.Controllers.APIs
                 _logService.LogWarning("Solicitação inválida para deletar SubMenu.");
                 return StatusCode(StatusCodes.Status400BadRequest, "Solicitação não foi possível de ser executada");
             }
-            catch (Exception ex)
-            {
-                _logService.LogError(ex, $"Erro ao deletar SubMenu: {ex.Message}");
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao deletar SubMenu: {ex.Message}");
-            }
+
+            _logService.LogWarning($"Acesso não autorizado à remoção de submenus pelo usuário {loggedUser.Item1}");
+            return Unauthorized("Não permitido");
+        }
+        catch (Exception ex)
+        {
+            _logService.LogError(ex, $"Erro ao deletar SubMenu: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao deletar SubMenu: {ex.Message}");
         }
     }
 }
