49c70d707c005e800ba446b4dd37a38722283935
andressa-fortuna <andressa.fortuna@keltecnologia.com.br>
2024-06-17 15:09:17 -0300

    criando campor km entre revisões no cadastro de veiculos

---
+++ backoffice/Infra/Migrations/20240617154505__v55.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class _v55 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KM_EntreRevisoes",
+                table: "Veiculo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KM_EntreRevisoes",
+                table: "Veiculo");
+        }
+    }
+}
