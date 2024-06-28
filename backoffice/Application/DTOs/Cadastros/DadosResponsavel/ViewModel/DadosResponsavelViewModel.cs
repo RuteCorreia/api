c10fd36f8ad780c600a849e4908be90baff071dd
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-06-28 13:25:40 -0300

    :hammer: arrumando id DadosResponsavel

--- backoffice/Application/DTOs/Cadastros/DadosResponsavel/ViewModel/DadosResponsavelViewModel.cs
+++ backoffice/Application/DTOs/Cadastros/DadosResponsavel/ViewModel/DadosResponsavelViewModel.cs
@@ -2,7 +2,7 @@
 
 public class DadosResponsavelViewModel
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string Data { get; set; }
     public string UF { get; set; }
     public string Cidade { get; set; }
