945983d02342ce28fb3564f061f4721459d79b00
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-07-02 14:08:41 -0300

    :hammer: ajustando ponto e virgular

--- backoffice/Domain/Interfaces/Cadastros/CombateIncendioPista/ICombateIncendioPistaRepository.cs
+++ backoffice/Domain/Interfaces/Cadastros/CombateIncendioPista/ICombateIncendioPistaRepository.cs
@@ -6,7 +6,7 @@
         Task<int> UpdateAsync(Entidades.Cadastros.CombateIncendio.CombateIncendioPista obj);
         Task DeleteAsync(int id);
         Task<IEnumerable<Entidades.Cadastros.CombateIncendio.CombateIncendioPista>> GetAllAsync(int? idEmpresa);
-        Task<IEnumerable<Domain.Entidades.Cadastros.CombateIncendio.CombateIncendioPista>> GetByCombateIncendioIdAsync(int combateIncendioId)
+        Task<IEnumerable<Domain.Entidades.Cadastros.CombateIncendio.CombateIncendioPista>> GetByCombateIncendioIdAsync(int combateIncendioId);
         Task<Entidades.Cadastros.CombateIncendio.CombateIncendioPista> GetByIdAsync(int id);
     }
 }
