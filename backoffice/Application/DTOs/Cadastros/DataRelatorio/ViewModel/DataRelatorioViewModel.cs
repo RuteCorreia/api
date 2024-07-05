d4d8e67499950bc481283bb8a3ee06924244b16f
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-07-05 11:35:18 -0300

    :hammer: ajustando viewModel data relatorio

--- backoffice/Application/DTOs/Cadastros/DataRelatorio/ViewModel/DataRelatorioViewModel.cs
+++ backoffice/Application/DTOs/Cadastros/DataRelatorio/ViewModel/DataRelatorioViewModel.cs
@@ -5,7 +5,7 @@ namespace Application.DTOs.Cadastros.DataRelatorio.ViewModel
 {
     public class DataRelatorioViewModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Data { get; set; }
         public int? IdEmpresa { get; set; }
     }
