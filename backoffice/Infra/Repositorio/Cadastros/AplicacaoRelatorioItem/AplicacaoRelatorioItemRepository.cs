c28cd5fb742a237184639345e2d2a3cec005fc8a
Evaldo <evaldo@parser.com.br>
2024-07-08 12:28:10 -0300

    Restaurar função de obter pelo relatorio aplicação.

--- backoffice/Infra/Repositorio/Cadastros/AplicacaoRelatorioItem/AplicacaoRelatorioItemRepository.cs
+++ backoffice/Infra/Repositorio/Cadastros/AplicacaoRelatorioItem/AplicacaoRelatorioItemRepository.cs
@@ -45,11 +45,11 @@ public class AplicacaoRelatorioItemRepository : IAplicacaoRelatorioItemRepositor
 
     public async Task<IEnumerable<Domain.Entidades.Cadastros.Aplicacao.AplicacaoRelatorioItem>> GetAllByAplicacaoRelatorioIdAsync(int idAplicacaoRelatorio)
     {
-        //var entities = await _contextBase.AplicacaoRelatorioItem.Where(aplicacaoRelatorioItem=> aplicacaoRelatorioItem.IdAplicacaoRelatorio == aplicacaoRelatorioId).ToListAsync();
-        //return entities;
+        var entities = await _contextBase.AplicacaoRelatorioItem.Where(aplicacaoRelatorioItem=> aplicacaoRelatorioItem.IdAplicacaoRelatorio == idAplicacaoRelatorio).ToListAsync();
+        return entities;
 
-        string query = "SELECT * FROM AplicacaoRelatorioItem WHERE IdAplicacaoRelatorio = @IdAplicacaoRelatorio";
-        return await _dbConnection.QueryAsync<Domain.Entidades.Cadastros.Aplicacao.AplicacaoRelatorioItem>(query, new { IdAplicacaoRelatorio = idAplicacaoRelatorio });
+        //string query = "SELECT * FROM AplicacaoRelatorioItem WHERE IdAplicacaoRelatorio = @IdAplicacaoRelatorio";
+        //return await _dbConnection.QueryAsync<Domain.Entidades.Cadastros.Aplicacao.AplicacaoRelatorioItem>(query, new { IdAplicacaoRelatorio = idAplicacaoRelatorio });
     }
 
 
