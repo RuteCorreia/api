94eea31899e16c50643dcf84a6bf561bdeebc1d8
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-07-11 18:22:35 -0300

    :hammer: adicionando migration ao banco para criar tabela TipoDeServico

---
+++ backoffice/Domain/Entidades/Cadastros/TipoDeServico/TipoDeServico.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entidades.Cadastros.TipoDeServico
+{
+    public class TipoDeServico
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? NomeServico { get; set; }
+    }
+}
