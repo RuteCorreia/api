b5cde7ce5350bd1afe475954deb59db1f2052b52
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-07-12 11:59:18 -0300

    :hammer: erro

--- backoffice/Infra/Repositorio/Cadastros/Contratante/ContratanteRepository.cs
+++ backoffice/Infra/Repositorio/Cadastros/Contratante/ContratanteRepository.cs
@@ -50,20 +50,22 @@ namespace Infra.Repositorio.Cadastros.Contratante
 
         public async Task<Domain.Entidades.Cadastros.Contratante.Contratante> GetByIdAsync(int? id)
         {
-            using (var connection = _dbConnection)
-            {
-                try
-                {
-                    string query = $"SELECT * FROM Contratante WHERE Id = {id}";
-                    var contratante = await connection.QueryFirstOrDefaultAsync<Domain.Entidades.Cadastros.Contratante.Contratante>(query);
-                    return contratante;
+            var obj = await _contextBase.Contratante.FindAsync(id);
+            return obj;
+            //using (var connection = _dbConnection)
+            //{
+            //    try
+            //    {
+            //        string query = $"SELECT * FROM Contratante WHERE Id = {id}";
+            //        var contratante = await connection.QueryFirstOrDefaultAsync<Domain.Entidades.Cadastros.Contratante.Contratante>(query);
+            //        return contratante;
 
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception(ex.Message);
-                }
-            }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        throw new Exception(ex.Message);
+            //    }
+            //}
         }
 
         public async Task UpdateAsync(Domain.Entidades.Cadastros.Contratante.Contratante obj)
