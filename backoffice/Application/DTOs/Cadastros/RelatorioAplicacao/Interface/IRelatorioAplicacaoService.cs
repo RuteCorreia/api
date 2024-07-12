88983df6500f1708969275ccaad4a6a1a2d5e801
andressa-fortuna <andressa.fortuna@keltecnologia.com.br>
2024-07-12 17:01:28 -0300

    ajustando endpoint getrelatorioMapa para relatorios de aplicação

--- backoffice/Application/DTOs/Cadastros/RelatorioAplicacao/Interface/IRelatorioAplicacaoService.cs
+++ backoffice/Application/DTOs/Cadastros/RelatorioAplicacao/Interface/IRelatorioAplicacaoService.cs
@@ -8,9 +8,10 @@ public interface IRelatorioAplicacaoService
     Task<RelatorioAplicacaoViewModel> AddAsync(RelatorioAplicacaoViewModel obj, string? idEmpresa);
     Task<IEnumerable<RelatorioAplicacaoViewModel>> GetByDataCriacaoAsync(DateTime Date, string? idEmpresa);
     Task<IEnumerable<RelatorioAplicacaoViewModel>> GetListByStatusAsync(string? idEmpresa);
+    Task<IEnumerable<RelatorioAplicacaoViewModel>> GetListByStatusMapaAsync(string? idEmpresa);
     Task<IEnumerable<RelatorioAplicacaoViewModel>> GetByDataAlteracaoAsync(DateTime Date, string? idEmpresa);
     Task<IEnumerable<RelatorioAplicacaoViewModel>> GetNovosAsync(DateTime? date, string? idEmpresa);
     Task UpdateAsync(RelatorioAplicacaoViewModel obj);
-    Task UpdateIsMapaAsync(RelatorioAplicacaoViewModel obj);
+    Task UpdateIsMapaAsync(List<RelatorioAplicacaoViewModel> obj);
     Task DeleteAsync(int id);
 }
--- backoffice/Infra/Repositorio/Cadastros/RelatorioAplicacao/RelatorioAplicacaoRepository.cs
+++ backoffice/Infra/Repositorio/Cadastros/RelatorioAplicacao/RelatorioAplicacaoRepository.cs
@@ -112,6 +112,12 @@ namespace Infra.Repositorio.Cadastros.RelatorioAplicacao
             string query = "SELECT * FROM RelatorioAplicacao WHERE IdEmpresa = @IdEmpresa AND StatusEnvio =  @statusEnvio  AND IsMapa = 0";
             return await _dbConnection.QueryAsync<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao>(query, new { IdEmpresa = idEmpresa, StatusEnvio = statusEnvio });
         }
+        public async Task<IEnumerable<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao>> GetListByStatusMapaAsync(int idEmpresa, int statusEnvio)
+        {
+            string query = "SELECT * FROM RelatorioAplicacao WHERE IdEmpresa = @IdEmpresa AND StatusEnvio =  @statusEnvio  AND IsMapa = 1";
+            return await _dbConnection.QueryAsync<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao>(query, new { IdEmpresa = idEmpresa, StatusEnvio = statusEnvio });
+        }
+
         public async Task<IEnumerable<Domain.Entidades.Cadastros.RelatorioAplicacao.RelatorioAplicacao>> GetAllByIdEmpresaAsync(int idEmpresa)
         {
             string query = "SELECT * FROM RelatorioAplicacao WHERE IdEmpresa = @IdEmpresa";
