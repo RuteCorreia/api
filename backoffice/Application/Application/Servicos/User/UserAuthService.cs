c72207f09d4817482b58ed277afb993396ec5d04
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-07-25 10:13:46 -0300

    :hammer: ajuste para validar se o usuario é empresa

--- backoffice/Application/Application/Servicos/User/UserAuthService.cs
+++ backoffice/Application/Application/Servicos/User/UserAuthService.cs
@@ -16,35 +16,41 @@ using System.Security.Claims;
 using System.Text;
 using System.Web;
 using System.Text.RegularExpressions;
+using Domain.Interfaces.Cadastros.Empresa;
 
 namespace Application.Application.Servicos.User;
 
 public class UserAuthService : IUserAuthService
 {
+    private readonly IUsuarioCredencialRepository _usuarioCredencialRepository;
     private readonly UserManager<IdentityUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
-    private readonly IConfiguration _config;
     private readonly IUsuarioRepository _usuarioRepository;
-    private readonly IUsuarioCredencialRepository _usuarioCredencialRepository;
-    private readonly IMapper _mapper;
+    private readonly IEmpresaRepository _empresaRepository;
     private readonly IEmailService _emailService;
+    private readonly IConfiguration _config;
+    private readonly IMapper _mapper;
+    
     public UserAuthService(
+        IUsuarioCredencialRepository usuarioCredencialRepository,
         UserManager<IdentityUser> userManager, 
         RoleManager<IdentityRole> roleManager,
-        IConfiguration config, 
         IUsuarioRepository usuarioRepository,
-        IUsuarioCredencialRepository usuarioCredencialRepository,
-        IMapper mapper,
-        IEmailService emailService
+        IEmpresaRepository empresaRepository,
+        IEmailService emailService,
+        IConfiguration config,   
+        IMapper mapper
+        
         )
     {
+        _usuarioCredencialRepository = usuarioCredencialRepository;
+        _usuarioRepository = usuarioRepository;
+        _empresaRepository = empresaRepository;
+        _emailService = emailService;
         _userManager = userManager;
         _roleManager = roleManager;
-        _config = config;
-        _usuarioRepository = usuarioRepository;
-        _usuarioCredencialRepository = usuarioCredencialRepository;
         _mapper = mapper;
-        _emailService = emailService;
+        _config = config;     
     }
 
     public async Task<(bool, string)> LoginAsync(UserLoginViewModel user)
@@ -288,6 +294,10 @@ public class UserAuthService : IUserAuthService
         var userToUpdate = await GetUserEntityByIdAsync(id);
         if (userToUpdate is not null)
         {
+            var isEmpresa = await _empresaRepository.GetByEmailAsync(userToUpdate.Email);
+            if (isEmpresa != null)
+                return (false, resultMsg.Clear().Append("O Email é uma empresa, você não pode editar nessa área").ToString());
+
             var identityUser = await _userManager.FindByEmailAsync(userToUpdate.Email);
             if(identityUser is not null)
             {
