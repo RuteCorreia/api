8a5dc437f9649cc242d13c576622e0cc8c4e0254
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-08-15 09:54:10 -0300

    :hammer: ajustando observação na viewModel

--- backoffice/Application/DTOs/Cadastros/Controle De Frota/ViewModel/ControleDeFrotaViewModel.cs
+++ backoffice/Application/DTOs/Cadastros/Controle De Frota/ViewModel/ControleDeFrotaViewModel.cs
@@ -14,6 +14,9 @@ public class ControleDeFrotaViewModel
     [JsonProperty("placaVeiculo")]
     public string? NomeVeiculo { get; set; }
 
+    [JsonProperty("observacao")]
+    public string? Observacao { get; set; }
+
     [JsonProperty("createdAt")]
     public DateTime? DataCriacao { get; set; }
 
