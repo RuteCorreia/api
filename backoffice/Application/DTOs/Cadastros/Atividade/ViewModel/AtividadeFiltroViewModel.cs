7fe4d84ee5532e1862ee1f6bd74b2432e010904a
lucas lazzari <lucas.lazzari@keltecnologia.com.br>
2024-08-16 10:02:22 -0300

    :hammer: ajuste json property

--- backoffice/Application/DTOs/Cadastros/Atividade/ViewModel/AtividadeFiltroViewModel.cs
+++ backoffice/Application/DTOs/Cadastros/Atividade/ViewModel/AtividadeFiltroViewModel.cs
@@ -1,12 +1,25 @@
-﻿namespace Application.DTOs.Cadastros.Atividade.ViewModel
+﻿using Newtonsoft.Json;
+
+namespace Application.DTOs.Cadastros.Atividade.ViewModel
 {
     public class AtividadeFiltroViewModel
     {
+        [JsonProperty("aeronave")]
         public string? PrefixoAeronave { get; set; }
+
+        [JsonProperty("piloto")]
         public string? Piloto { get; set; }
+
+        [JsonProperty("executor")]
         public string? Executor { get; set; }
+
+        [JsonProperty("cliente")]
         public string? Contratante { get; set; }
+
+        [JsonProperty("dataInicio")]
         public DateTime? DataInicial { get; set; }
+
+        [JsonProperty("dataFim")]
         public DateTime? DataFinal { get; set; }
     }
 }
