44f73f75c6c03dfd5635b86703d60da6ac6764a1
lucaslazzari <lucas.lazzari@keltecnologia.com.br>
2024-10-07 13:02:04 -0300

    :hammer: :fix: GetPistaApp

---
+++ backoffice/Application/DTOs/Cadastros/Pistas/ViewModel/PistaAppViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Cadastros.Pistas.ViewModel
+{
+    public class PistaAppViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double LAT { get; set; }
+        public double LONG { get; set; }
+        public int? IdEmpresa { get; set; }
+    }
+}
