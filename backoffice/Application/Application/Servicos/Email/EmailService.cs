7d8c543e7b0becc77bf78f590c4ad09371830681
lucaslazzari <lucas.lazzari@keltecnologia.com.br>
2024-11-05 17:38:20 -0300

    :hammer: :fix: voltando para porta anterior no disparo de email

--- backoffice/Application/Application/Servicos/Email/EmailService.cs
+++ backoffice/Application/Application/Servicos/Email/EmailService.cs
@@ -97,9 +97,8 @@ public class EmailService : IEmailService
             // CONFIGURAÇÃO DO EMAIL
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             SmtpClient client = new SmtpClient();
-            client.UseDefaultCredentials = false;
             client.Credentials = new NetworkCredential("atendimento@keltech.app", "mbfepucevpscjijz"); //colocar credenciais da flytec (essas são do eag)
-            client.Port = 465; //verificar se essa porta esta correta
+            client.Port = 25; //verificar se essa porta esta correta
             client.Host = "smtp.gmail.com";
             client.EnableSsl = true;
             //enviar
