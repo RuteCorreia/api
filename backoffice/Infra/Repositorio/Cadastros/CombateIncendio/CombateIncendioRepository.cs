796b1f7cd5e1b96eff1c28365d4fbbff2c9734b1
lucaslazzariKL <lucas.lazzari@keltecnologia.com.br>
2024-12-10 10:23:04 -0300

    :hammer: :fix: ajuste minhas atividades

--- backoffice/Infra/Repositorio/Cadastros/CombateIncendio/CombateIncendioRepository.cs
+++ backoffice/Infra/Repositorio/Cadastros/CombateIncendio/CombateIncendioRepository.cs
@@ -233,7 +233,7 @@ public class CombateIncendioRepository : ICombateIncendioRepository
 	            LEFT JOIN Aeronave a ON c.IdAeronave = a.Id
 	            JOIN Usuario u ON c.IdExecutor = u.Id
             WHERE (a.Prefixo LIKE '%' + @PrefixoAeronave + '%' OR c.IdAeronave IS NULL)
-                WHERE cps.Extensao IS NOT NULL AND cps.Extensao != ''
+                AND cps.Extensao IS NOT NULL AND cps.Extensao != ''
 	            AND c.Piloto LIKE '%' + @Piloto + '%'
 	            AND u.Nome LIKE '%' + @Executor + '%'
 	            AND c.Cliente LIKE '%' + @Cliente + '%'
