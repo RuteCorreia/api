218c1572e72ec8c9cbcc15a26caaf01962d462f8
Suporte Fly Tec <suporte_flytecsa.com.br@contatoflytecsagmail.onmicrosoft.com>
2025-08-09 12:33:38 -0300

    ajuste validações usuarios e empresas para fazer login - empresas/usuarios removidos/inativados.

--- backoffice/Application/Application/Servicos/User/UserAuthService.cs
+++ backoffice/Application/Application/Servicos/User/UserAuthService.cs
@@ -60,56 +60,105 @@ public class UserAuthService : IUserAuthService
         if (identityUser is not null)
         {
             var usuario = await _usuarioRepository.GetByUserIdAsync(identityUser.Id);
-            if (usuario is not null)
-            {
-                var passwordCheck = await _userManager.CheckPasswordAsync(identityUser, user.Password);
-                if (passwordCheck)
-                {
-                    var token = new StringBuilder();
+            if (usuario is not null) {
+                if (usuario.Removido) {
+                    return (false, "Usuário removido. Entre em contato com o administrador do sistema.");
+                };
 
-                    if (usuario.PrimeiroAcesso)
-                    {
-                        usuario.PrimeiroAcesso = false;
-                        await _usuarioRepository.UpdateAsync(usuario);
-                    }
-                    token.Append(await GenerateToken(identityUser, usuario));
+                if (usuario.IdEmpresa is not null) {
+                    var empresa = await _empresaRepository.GetByIdAsync(usuario.IdEmpresa);
 
-                    return (true, token.ToString());
-                }
-            }
-        }
-        return (false, "login inválido");
+                    if (empresa is null) {
+                        return (false, "Empresa vinculada ao usuário não foi encontrada. Entre em contato com o administrador do sistema.");
+
+                    } else {
+                        if (empresa.Status is not 0) {
+                            return (false, "Empresa desativada. Entre em contato com o administrador do sistema.");
+
+                        } else {
+                            var passwordCheck = await _userManager.CheckPasswordAsync(identityUser, user.Password);
+                            if (passwordCheck) {
+                                var token = new StringBuilder();
+
+                                if (usuario.PrimeiroAcesso) {
+                                    usuario.PrimeiroAcesso = false;
+                                    await _usuarioRepository.UpdateAsync(usuario);
+                                }
+
+                                token.Append(await GenerateToken(identityUser, usuario));
+                                return (true, token.ToString());
+
+                            } else {
+                                return (false, "Senha inválida, por favor tente novamente.");
+                            };
+                        };
+                    };
+
+                } else {
+                    return (false, "Usuário não possui empresa vinculada. Entre em contato com o administrador do sistema.");
+                };
+
+            };
+
+            return (false, "Usuário não encontrado.");
+
+        };
+        return (false, "Usuário não encontrado.");
     }
 
     public async Task<(bool, string, IList<string>, int?)> LoginBackofficeAsync(UserLoginViewModel user)
     {
         var identityUser = await _userManager.FindByEmailAsync(user.Email);
-        if (identityUser is not null)
-        {
+        if (identityUser is not null) {
             var roles = await _userManager.GetRolesAsync(identityUser);
-            if (roles.Contains("Administrativo") || roles.Contains("Administrador"))
-            {
-                var usuario = await _usuarioRepository.GetByUserIdAsync(identityUser.Id);
-                if (usuario is not null)
-                {
-                    var passwordCheck = await _userManager.CheckPasswordAsync(identityUser, user.Password);
-                    if (passwordCheck)
-                    {
-                        var token = new StringBuilder();
+            var usuario = await _usuarioRepository.GetByUserIdAsync(identityUser.Id);
 
-                        if (usuario.PrimeiroAcesso)
-                        {
-                            usuario.PrimeiroAcesso = false;
-                            await _usuarioRepository.UpdateAsync(usuario);
-                        }
-                        token.Append(await GenerateToken(identityUser, usuario));
+            if (usuario is not null) {
+                if (usuario.Removido) {
+                    return (false, "Usuário removido. Entre em contato com o administrador do sistema.", [], null);
+                };
 
-                        return (true, token.ToString(), roles, usuario.IdEmpresa);
-                    }
-                }
-            }
-        }
-        return (false, "login inválido", [], null);
+                if (usuario.IdEmpresa is not null) {
+                    var empresa = await _empresaRepository.GetByIdAsync(usuario.IdEmpresa);
+
+                    if (empresa is null) {
+                        return (false, "Empresa vinculada ao usuário não foi encontrada. Entre em contato com o administrador do sistema.", [], null);
+
+                    } else {
+                        if (empresa.Status is not 0) {
+                            return (false, "Empresa desativada. Entre em contato com o administrador do sistema.", [], null);
+
+                        } else {
+                            if (roles.Contains("Administrativo") || roles.Contains("Administrador")) {
+                                var token = new StringBuilder();
+                                token.Append(await GenerateToken(identityUser, usuario));
+
+                                return (true, token.ToString(), roles, usuario.IdEmpresa);
+
+                            } else {
+                                var passwordCheck = await _userManager.CheckPasswordAsync(identityUser, user.Password);
+
+                                if (passwordCheck) {
+                                    var token = new StringBuilder();
+                                    token.Append(await GenerateToken(identityUser, usuario));
+
+                                    return (true, token.ToString(), roles, usuario.IdEmpresa);
+
+                                } else {
+                                    return (false, "Senha incorreta. Por favor, tente novamente.", [], null);
+                                };
+                            };
+                        };
+                    };
+                } else {
+                    return (false, "Usuário não possui empresa vinculada. Entre em contato com o administrador do sistema.", [], null);
+                };
+            };
+
+            return (false, "Usuário não encontrado.", [], null);
+        };
+
+        return (false, "Usuário não encontrado.", [], null);
     }
 
     public async Task<(bool, string)> RegisterUserAsync(UserRegisterViewModel request, string loggedUserId)
