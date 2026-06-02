132894a785f658934d86bc64872dfa02b96bdf66
rute1107 <rute1107@gmail.com>
2026-06-01 23:12:07 -0300

    alterar data relatorio de faturamento

--- backoffice/Infra/Repositorio/Cadastros/Dashboard/DashboardRepository.cs
+++ backoffice/Infra/Repositorio/Cadastros/Dashboard/DashboardRepository.cs
@@ -175,7 +175,7 @@ namespace Infra.Repositorio.Cadastros.Dashboard
 	                        ra.Piloto AS Piloto,
 	                        ra.Executor AS Executor,
 	                        c.Nome as Cliente,
-	                        CONVERT(DATE, ra.DataCriacao) AS DataCriacao,
+	                        COALESCE(CAST(DATEADD(SECOND, TRY_CAST(dr.Data AS BIGINT) / 1000, '1970-01-01') AS DATE), CONVERT(DATE, ra.DataCriacao)) AS DataCriacao,
 	                        LEFT(ar.NomeAeronave, 
 		                        CASE 
 			                        WHEN CHARINDEX(' - ', ar.NomeAeronave) > 0 
@@ -201,6 +201,8 @@ namespace Infra.Repositorio.Cadastros.Dashboard
                             AplicacaoRelatorio are ON ra.AplicacaoRelatorioId = are.Id
                         JOIN
                             Contratante c ON ra.ContratanteId = c.Id
+                        LEFT JOIN
+                            DadosResponsavel dr ON ra.DadosResponsavelId = dr.Id
                         WHERE
                             ra.IdEmpresa = @IdEmpresa
                             AND ra.StatusEnvio = 0
